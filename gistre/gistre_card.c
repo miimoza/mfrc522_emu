d33ed882ea232a6f729f80ec18b660faeb59d51a
Antoine Coulon <antoine.coulon@epita.fr>
2020-06-22 20:06:47 +0200

    asd

--- gistre/gistre_card.c
+++ gistre/gistre_card.c
@@ -11,25 +11,25 @@ __init
 static int gistre_card_init(void) {
 	pr_info("Hello, GISTRE card !\n");
 
-	struct device *dev = mfrc522_find_dev();
-	struct mfrc522_dev *mfrc = dev_to_mfrc522(dev);
-	struct regmap *rm = mfrc522_get_regmap(mfrc);
+	struct device *device;
+	struct mfrc522_dev *mfrc522_dev;
+	struct regmap *regmap;
+	struct device_node *device_node;
 
+	device = mfrc522_find_dev();
+	mfrc522_dev = dev_to_mfrc522(device);
+	regmap = mfrc522_get_regmap(mfrc522_dev);
+	device_node = of_find_node_by_name(of_root, MFRC522_NAME);
 
-	pr_info("cmdlock: %d\n", mfrc->cmd_lock);
+	//pr_info("cmdlock: %d\n", mfrc->cmd_lock);
 
-	unsigned int cmd_reg_cmd;
-	regmap_read(rm, MFRC522_CMDREG_CMD, &cmd_reg_cmd);
+	//unsigned int cmd_reg_cmd;
+	//regmap_read(rm, MFRC522_CMDREG_CMD, &cmd_reg_cmd);
+	//pr_info("cmd reg cmd: %d\n", cmd_reg_cmd);
 
-	pr_info("cmd reg cmd: %d\n", cmd_reg_cmd);
-
-	struct device_node *dev_node = of_find_node_by_name(of_root, MFRC522_NAME);
 	const char *version;
 	of_property_read_string(dev_node, MFRC522_PROP_VERSION, &version);
 	pr_info("Version: %s\n", version);
-
-
-
 
 	return 0;
 }
