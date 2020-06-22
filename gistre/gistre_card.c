b67b4386c7b3e9efa0af658c84ffdbadd503aa27
Antoine Coulon <antoine.coulon@epita.fr>
2020-06-22 19:18:10 +0200

    asd

--- gistre/gistre_card.c
+++ gistre/gistre_card.c
@@ -1,9 +1,24 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <mfrc522.h>
+#include <linux/include/linux/regmap.h>
 
 __init
 static int gistre_card_init(void) {
 	pr_info("Hello, GISTRE card !\n");
+
+	struct device *dev = mfrc522_find_dev();
+	struct mfrc522_dev *mfrc = dev_to_mfrc522(dev);
+	struct regmap *rm = mfrc522_get_regmap(mfrc);
+
+
+	pr_info("cmdlock: %d\n", mfrc->cmd_lock);
+
+	unsigned int cmd_reg_cmd;
+	regmap_read(rm, MFRC522_CMDREG_CMD, &cmd_reg_cmd);
+
+	pr_info("cmd reg cmd: %d\n", cmd_reg_cmd);
+
 	return 0;
 }
 
