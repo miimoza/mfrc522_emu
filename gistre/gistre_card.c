6fe1b0433d62c64c0f2026dd13bf17afe552fb12
Antoine Coulon <antoine.coulon@epita.fr>
2020-06-22 21:04:02 +0200

    asd

--- gistre/gistre_card.c
+++ gistre/gistre_card.c
@@ -1,9 +1,15 @@
+#include <linux/cdev.h>
+#include <linux/errno.h>
+#include <linux/fs.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include "mfrc522.h"
-#include "mfrc522_emu.h"
+#include <linux/slab.h>
+#include <linux/uaccess.h>
 #include <linux/regmap.h>
 #include <linux/of.h>
+
+#include "mfrc522.h"
+#include "mfrc522_emu.h"
 
 #define MFRC522_NAME "mfrc522_emu"
 #define MFRC522_PROP MFRC522_PROP_VERSION
@@ -12,13 +18,81 @@
  * Metadata
  */
 
-MODULE_DESCRIPTION("A driver for the dummy \"Mfrc522\" device node");
-MODULE_AUTHOR("Caproute <cap.route@gmail.com>");
+MODULE_DESCRIPTION("Cardio: a driver for the dummy \"Mfrc522\" iHaospace Kit RFID RC522, Module Carte de Lecteur MFRC-522 RF IC avec RFID Carte Puce Chip S50 13.56MHz pour Mifare Arduino Raspberry Pi device node");
+MODULE_AUTHOR("Caproute <cap.route@freemail.bcd>");
 MODULE_LICENSE("GPL v2");
 
+/*
+ * Local definitions
+ */
+
+struct cardio_dev {
+	struct cdev cdev;
+};
+
+/* Major will always be dynamically allocated */
+static int major;
+static struct cardio_dev *cio_dev;
+
+/*
+ *  Init & Exit
+ */
+
+ static struct file_operations cardio_fops = {
+ 	.owner   = THIS_MODULE
+ 	/* Others functions are using the kernel's defaults */
+ };
+
+static struct cardio_dev *cardio_create(void) {
+
+	struct cardio_dev *dev = kmalloc(sizeof(*dev), GFP_KERNEL);
+	if (! dev) {
+		return NULL;
+	}
+
+	dev->cdev.owner = THIS_MODULE;
+	cdev_init(&dev->cdev, &cardio_fops);
+
+	return dev;
+}
+
+static void cardio_destroy(struct cardio_dev *dev) {
+	cdev_del(&dev->cdev);
+	kfree(dev);
+}
+
 __init
-static int gistre_card_init(void) {
+static int cardio_init(void) {
 	pr_info("Hello, GISTRE card !\n");
+
+	dev_t dev;
+	int ret;
+	const char devname[] = "cardio";
+
+	/* Allocate major */
+	ret = alloc_chrdev_region(&dev, 0, 1, devname);
+	if (ret < 0) {
+		pr_info("Failed to allocate major\n");
+		return 1;
+	}
+	else {
+		major = MAJOR(dev);
+		pr_info("Got major %d\n", major);
+	}
+
+	/* Register char device */
+	cio_dev = pingpong_create();
+	if (!cio_dev) {
+		pr_err("Failed to init pingpong_dev\n");
+		return -ENOMEM;
+	}
+
+	if (cdev_add(&pp_dev->cdev, dev, 1) < 0) {
+		pr_err("Failed to register char device\n");
+		return -ENOMEM;
+	}
+
+
 
 	struct device *device;
 	struct mfrc522_dev *mfrc522_dev;
@@ -48,9 +122,15 @@ static int gistre_card_init(void) {
 }
 
 __exit
-static void gistre_card_exit(void) {
+static void cardio_exit(void) {
+	dev_t dev;
+
+	dev = MKDEV(major, 0);
+	pingpong_destroy(cio_dev);
+	unregister_chrdev_region(dev, 1);
+
 	pr_info("Goodbye, GISTRE card !\n");
 }
 
-module_init(gistre_card_init);
-module_exit(gistre_card_exit);
+module_init(cardio_init);
+module_exit(cardio_exit);
