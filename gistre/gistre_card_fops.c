2b3690966eaef07e1d76d3134bb3d7005eea7f94
Antoine Coulon <antoine.coulon@epita.fr>
2020-06-22 21:33:17 +0200

    asd

--- gistre/gistre_card_fops.c
+++ gistre/gistre_card_fops.c
@@ -8,7 +8,7 @@ struct file_operations cardio_fops = {
 };
 
 ssize_t cardio_read(struct file *file, char __user *buf,
-    size_t len, loff_t *off /* unused */) {
+    size_t len, loff_t *off /* unused */)
 {
     pr_info("reax\n");
     return 0;
