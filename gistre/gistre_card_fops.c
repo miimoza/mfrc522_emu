ae0bba9745fecd51215f6847c29e77224a34b33f
Antoine Coulon <antoine.coulon@epita.fr>
2020-06-22 21:24:04 +0200

    asd

--- gistre/gistre_card_fops.c
+++ gistre/gistre_card_fops.c
@@ -1,9 +1,9 @@
 #include "gistre_card_fops.h"
 
 struct file_operations cardio_fops = {
- 	.owner   = THIS_MODULE
+ 	.owner   = THIS_MODULE,
     .read    = cardio_read,
-    .write   = cardio_write,
+    .write   = cardio_write
  	/* Others functions are using the kernel's defaults */
 };
 
