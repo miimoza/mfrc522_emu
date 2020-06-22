73a9b3a118f8cdd2fcd6d7c36ebf48c624688431
Antoine Coulon <antoine.coulon@epita.fr>
2020-06-22 21:30:03 +0200

    asd

--- gistre/gistre_card_fops.h
+++ gistre/gistre_card_fops.h
@@ -6,6 +6,10 @@
 #include <linux/fs.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/regmap.h>
+#include <linux/of.h>
 
 struct file_operations cardio_fops;
 
