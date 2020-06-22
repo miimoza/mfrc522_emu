d29cdfbd20fd94614eb6558977b23f381ad7acff
Antoine Coulon <antoine.coulon@epita.fr>
2020-06-22 19:57:20 +0200

    asd

--- gistre/gistre_card.c
+++ gistre/gistre_card.c
@@ -24,9 +24,9 @@ static int gistre_card_init(void) {
 	pr_info("cmd reg cmd: %d\n", cmd_reg_cmd);
 
 	struct device_node *dev_node = of_find_node_by_name(of_root, MFRC522_NAME);
-	u64 version;
-	of_property_read_u64(dev_node, MFRC522_PROP_VERSION, &version);
-	pr_info("Version: %d\n", version);
+	char *version;
+	of_property_read_string(dev_node, MFRC522_PROP_VERSION, &version);
+	pr_info("Version: %s\n", version);
 
 
 
