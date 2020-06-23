eda504310ad4727bf0f3a96c52fe441091653c54
Antoine Coulon <antoine.coulon@epita.fr>
2020-06-23 17:19:37 +0200

    test

--- gistre/card/card_read.c
+++ gistre/card/card_read.c
@@ -13,19 +13,20 @@ ssize_t card_read(struct file *file, char __user *buf, size_t len,
 
 
 
-	// cdev = (struct pingpong_dev *)file->private_data;
 	if (c_dev->buffer_len) {
-
         len = min(len, c_dev->buffer_len);
 		if (copy_to_user(buf, c_dev->buffer, c_dev->buffer_len)) {
 			pr_err("Failed to copy data to user\n");
 			return -EFAULT;
 		}
+        len = c_dev->buffer_len;
+        c_dev->buffer_len = 0;
+        c_dev->buffer[0] = '\0';
 	}
 	else {
         pr_info("%s():Nothing in the buffer\n", __func__);
 		len = 0;
 	}
 
-	return c_dev->buffer_len;
+	return len;
 }
