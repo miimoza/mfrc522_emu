93a514593f24dc1e258067cbf39c1f2bacc401e4
Antoine Coulon <antoine.coulon@epita.fr>
2020-06-23 14:49:27 +0200

    test

--- gistre/card/card.h
+++ gistre/card/card.h
@@ -11,14 +11,23 @@
 #define MFRC522_NAME "mfrc522_emu"
 #define MFRC522_PROP MFRC522_PROP_VERSION
 
+
+// Metadata
+MODULE_DESCRIPTION("CARD DRIVER MRFC522: RFID RC522, Module MFRC-522 RF IC RFID S50 13.56MHz");
+MODULE_AUTHOR("Dorian Vinai <dorian.vinai@epita.fr>, Antoine Coulon <antoine.coulon@epita.fr>");
+MODULE_LICENSE("GPL v2");
+
 struct card_dev {
     struct cdev cdev;
 };
 
+int major;
+struct card_dev *cio_dev;
+
 __init int card_init(void);
 __exit void card_exit(void);
 
-int major;
-struct card_dev *cio_dev;
+module_init(card_init);
+module_exit(card_exit);
 
 #endif
