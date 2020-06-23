849921be5fdd030b54465a59e83130880d7a6afd
Antoine Coulon <antoine.coulon@epita.fr>
2020-06-23 17:45:34 +0200

    READ_WRITE_PRETTY_OK_STILL2CLEAN

--- p2.sh
+++
@@ -1,4 +0,0 @@
-mknod /dev/card c 255 0
-echo -n mem_write:5:hello > /dev/card
-echo -n mem_read > /dev/read
-cat /dev/card
--- p3.sh
+++
@@ -1,4 +0,0 @@
-mknod /dev/card c 255 0
-echo -n gen_rand_id >/dev/card
-echo -n mem_read > /dev/read
-hexdump /dev/card
