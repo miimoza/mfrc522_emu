25acc0e73b4bda7be06de845adc5817d4e7331df
Dorian Vinai <dorian.vinai@epita.fr>
2020-06-27 18:21:44 +0200

    fixed len 2

--- gistre/card/card_write_mem_read.c
+++ gistre/card/card_write_mem_read.c
@@ -8,7 +8,8 @@ ssize_t mem_read(struct regmap *regmap)
     regmap_write(regmap, MFRC522_CMDREG, MFRC522_MEM);
     regmap_read(regmap, MFRC522_FIFOLEVELREG_LEVEL, &len);
 
-    len &= 0x7F;
+    len &= 0xFE;
+    len = len >> 1;
 
     pr_info("len: %u\n", len);
 
@@ -17,9 +18,9 @@ ssize_t mem_read(struct regmap *regmap)
         pr_info("reading from MFRC522_FIFODATAREG: ");
         regmap_read(regmap, MFRC522_FIFODATAREG, (void *)(&c_dev->buffer[i]));
         pr_info("%d\n", c_dev->buffer[i]);
+        c_dev->buffer_len = i;
         if (c_dev->buffer[i] == '\0')
             break;
-        c_dev->buffer_len = i;
     }
     pr_info("In c_dev->buffer_len:%d\n", c_dev->buffer_len);
     pr_info("Store %s in c_dev->buffer\n", c_dev->buffer);
