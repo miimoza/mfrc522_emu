1dc789ef0ade4529dd19c8b5111c7caf0c885064
Antoine Coulon <antoine.coulon@epita.fr>
2020-06-27 18:45:00 +0200

    test

--- gistre/card/card_write_mem_write.c
+++ gistre/card/card_write_mem_write.c
@@ -8,7 +8,7 @@ void mem_write(struct regmap *regmap, const char __user *buf, size_t len)
     size_t i;
     for (i = 0; i < len; i++)
         regmap_write(regmap, MFRC522_FIFODATAREG, buf[i]);
-    regmap_write(regmap, MFRC522_FIFODATAREG, 0);
+    //regmap_write(regmap, MFRC522_FIFODATAREG, 0);
 
     regmap_write(regmap, MFRC522_CMDREG, MFRC522_IDLE);
 }
