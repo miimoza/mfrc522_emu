674e48e359a482289f435a28b3249411f57a7c28
Antoine Coulon <antoine.coulon@epita.fr>
2020-06-27 19:18:20 +0200

    test

--- gistre/card/card_write_mem_read.c
+++ gistre/card/card_write_mem_read.c
@@ -8,13 +8,16 @@ ssize_t mem_read(struct regmap *regmap)
     regmap_read(regmap, MFRC522_FIFOLEVELREG_LEVEL, &len);
 
 
+
     size_t i;
-    for (i = 0; i < len && i < 25; i++) {
+    //for (i = 0; i < len && i < 25; i++) {
+    for (i = 0; i < 25; i++)
         regmap_read(regmap, MFRC522_FIFODATAREG, (void *)(&c_dev->buffer[i]));
-        c_dev->buffer_len = i;
-        if (c_dev->buffer[i] == '\0')
-            break;
-    }
+
+    c_dev->buffer_len = i;
+    //if (c_dev->buffer[i] == '\0')
+    //    break;
+
 
     regmap_write(regmap, MFRC522_CMDREG, MFRC522_IDLE);
 
