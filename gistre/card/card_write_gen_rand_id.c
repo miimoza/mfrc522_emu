f4b9737e08e563910d72e8fdc178757076942112
Antoine Coulon <antoine.coulon@epita.fr>
2020-06-27 19:06:04 +0200

    test

--- gistre/card/card_write_gen_rand_id.c
+++ gistre/card/card_write_gen_rand_id.c
@@ -3,7 +3,7 @@
 void gen_rand_id(struct regmap *regmap)
 {
     regmap_write(regmap, MFRC522_CMDREG, MFRC522_MEM);
-    regmap_write(regmap, MFRC522_FIFOLEVELREG_FLUSH, 0x1);
+    //regmap_write(regmap, MFRC522_FIFOLEVELREG_FLUSH, 0x1);
 
     regmap_write(regmap, MFRC522_CMDREG, MFRC522_GENERATERANDOMID);
 
