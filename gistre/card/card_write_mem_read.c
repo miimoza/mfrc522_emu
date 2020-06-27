050c387c04f536cd5ce14abdd9603f0d2ef816a9
Dorian Vinai <dorian.vinai@epita.fr>
2020-06-27 18:14:12 +0200

    read

--- gistre/card/card_write_mem_read.c
+++ gistre/card/card_write_mem_read.c
@@ -11,13 +11,14 @@ ssize_t mem_read(struct regmap *regmap)
     pr_info("len: %u\n", len);
 
     size_t i;
-    for (i = 0; i < len && i < 64; i++) {
+    for (i = 0; i < len && i < 25; i++) {
         pr_info("reading from MFRC522_FIFODATAREG: ");
         regmap_read(regmap, MFRC522_FIFODATAREG, (void *)(&c_dev->buffer[i]));
         pr_info("%d\n", c_dev->buffer[i]);
+        if (c_dev->buffer[i] == '\0')
+            break;
         c_dev->buffer_len = i;
     }
-    c_dev->buffer[i] = '\0';
     pr_info("In c_dev->buffer_len:%d\n", c_dev->buffer_len);
     pr_info("Store %s in c_dev->buffer\n", c_dev->buffer);
 
